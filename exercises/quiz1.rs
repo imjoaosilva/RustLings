7c34c91402da5ea7bd32e4e98a65fe2e0946192b
Z4 <z4kreyyt@gmail.com>
2024-01-08 23:09:07 +0000

    Finishing quiz1.rs 👌

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,8 +13,6 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
 // fn calculate_price_of_apples {
 
@@ -30,4 +28,12 @@ fn verify_test() {
     assert_eq!(80, price2);
     assert_eq!(41, price3);
     assert_eq!(65, price4);
+}
+
+fn calculate_price_of_apples(n: u32) -> u32 {
+    if n > 40 {
+        return n
+    }
+
+    n*2
 }
