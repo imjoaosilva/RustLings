463c294cf6375a4834f1d1dbc1c675a11cd10adc
Z4 <z4kreyyt@gmail.com>
2024-01-09 20:12:37 +0000

    Finishing quiz2.rs 👌

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -20,8 +20,6 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
-
 pub enum Command {
     Uppercase,
     Trim,
@@ -32,12 +30,26 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
-        for (string, command) in input.iter() {
-            // TODO: Complete the function body. You can do it!
+        let mut output: Vec<String> = vec![];
+
+        for (mut string, command) in input {
+            match command {
+                Command::Uppercase => {
+                    output.push(string.to_uppercase());
+                }
+                Command::Trim => {
+                    output.push(string.trim().to_string());
+                }
+                Command::Append(pos) => {
+                    let bar = "bar".repeat(pos);
+                    string.push_str(bar.as_str());
+                    output.push(string);
+                }
+            }
         }
+
         output
     }
 }
@@ -45,17 +57,19 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
     fn it_works() {
-        let output = transformer(vec![
-            ("hello".into(), Command::Uppercase),
-            (" all roads lead to rome! ".into(), Command::Trim),
-            ("foo".into(), Command::Append(1)),
-            ("bar".into(), Command::Append(5)),
-        ]);
+        let output = transformer(
+            vec![
+                ("hello".into(), Command::Uppercase),
+                (" all roads lead to rome! ".into(), Command::Trim),
+                ("foo".into(), Command::Append(1)),
+                ("bar".into(), Command::Append(5))
+            ]
+        );
         assert_eq!(output[0], "HELLO");
         assert_eq!(output[1], "all roads lead to rome!");
         assert_eq!(output[2], "foobar");
