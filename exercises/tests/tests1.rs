fb4605880d10a331ea74fe0a3294c1fbdafe5554
Z4 <z4kreyyt@gmail.com>
2024-01-12 22:06:38 +0000

    Finishing all exercises (tests) ✨

--- exercises/tests/tests1.rs
+++ exercises/tests/tests1.rs
@@ -10,12 +10,11 @@
 // Execute `rustlings hint tests1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[cfg(test)]
 mod tests {
     #[test]
     fn you_can_assert() {
-        assert!();
+        let result = 1+1;
+        assert!(result > 1);
     }
 }
